589b85fab9aef2672092563f13689d3fd19ec57d
Nobel Yoo <nobel@digit.co>
2021-06-20 21:12:29 -0700

    add more art

--- generate_pages.py
+++ generate_pages.py
@@ -51,3 +51,81 @@ outputText = template.render({"gifs": gifs})
 text_file = open("output/telephone.html", "w")
 text_file.write(outputText)
 text_file.close()
+
+
+###### GLITCH #######
+# generate email pages
+sheetIndex = 4
+response = requests.get("https://spreadsheets.google.com/feeds/list/1X9FSpCIEoputo7chaNdjzyEcHunghZgOK6h4NtjmUpg/{}/public/values?alt=json".format(sheetIndex))
+sheet = json.loads(response.content)
+
+id = 0
+for row in sheet["feed"]["entry"]:
+    args = {
+        "to": row["gsx$to"]["$t"],
+        "from": row["gsx$from"]["$t"],
+        "subject": row["gsx$subject"]["$t"],
+        "body": row["gsx$body"]["$t"],
+    }
+    template = templateEnv.get_template("glitch_email.html")
+    outputText = template.render(args)
+    text_file = open("output/{}_email.html".format(id), "w")
+    text_file.write(outputText)
+    text_file.close()
+    id = id + 1
+
+
+# generate modal pages
+sheetIndex = 5
+response = requests.get("https://spreadsheets.google.com/feeds/list/1X9FSpCIEoputo7chaNdjzyEcHunghZgOK6h4NtjmUpg/{}/public/values?alt=json".format(sheetIndex))
+sheet = json.loads(response.content)
+
+id = 0
+for row in sheet["feed"]["entry"]:
+    args = {
+        "type": row["gsx$type"]["$t"],
+        "header": row["gsx$header"]["$t"],
+        "body": row["gsx$body"]["$t"],
+        "cta1": row["gsx$cta1"]["$t"],
+        "cta2": row["gsx$cta2"]["$t"],
+    }
+    template = templateEnv.get_template("glitch_modal.html")
+    outputText = template.render(args)
+    text_file = open("output/{}_modal.html".format(id), "w")
+    text_file.write(outputText)
+    text_file.close()
+    id = id + 1
+
+
+# generate chat pages
+sheetIndex = 6
+response = requests.get("https://spreadsheets.google.com/feeds/list/1X9FSpCIEoputo7chaNdjzyEcHunghZgOK6h4NtjmUpg/{}/public/values?alt=json".format(sheetIndex))
+sheet = json.loads(response.content)
+
+id = 0
+for row in sheet["feed"]["entry"]:
+    print(row.keys())
+    args = {
+        "person1": row["gsx$person1"]["$t"],
+        "person2": row["gsx$person2"]["$t"],
+        "messages": [
+            row["gsx$message1"]["$t"].split("/"),
+            row["gsx$message2"]["$t"].split("/"),
+            row["gsx$message3"]["$t"].split("/"),
+            row["gsx$message4"]["$t"].split("/"),
+            row["gsx$message5"]["$t"].split("/"),
+            row["gsx$message6"]["$t"].split("/"),
+            row["gsx$message7"]["$t"].split("/"),
+            row["gsx$message8"]["$t"].split("/"),
+            row["gsx$message9"]["$t"].split("/"),
+            row["gsx$message10"]["$t"].split("/"),
+            row["gsx$message11"]["$t"].split("/"),
+        ]
+    }
+    template = templateEnv.get_template("glitch_chat.html")
+    outputText = template.render(args)
+    text_file = open("output/{}_chat.html".format(id), "w")
+    text_file.write(outputText)
+    text_file.close()
+    id = id + 1
+
